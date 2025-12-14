75acd959b819cbe654bda7927c1a4f4dc3c37026
Karthikeyan088 <karthikeyan.kkm8@gmail.com>
2025-12-14 22:36:24 +0530

    Code added

--- Wall_Contain_Water/wall.java
+++ Wall_Contain_Water/wall.java
@@ -3,7 +3,48 @@ public class wall
     public static void main(String[]arg)
     {
         int[]wall={0,3,5,8,2,6,9};
-        
+        int n=wall.length;
+        int p1=0,p2=n-1;
+        int max=0;
+        int[]pair=new int[2];
+        while(p1<p2)
+        {
+            int leng=min(wall[p1],wall[p2]);
+            int brd=p2-p1;
+            int tot=leng*brd;
+            //   System.err.println(tot);
+            if(tot>max)
+            {
+                max=tot;
+                pair[0]=p1;
+                pair[1]=p2;
+
+            }
+            if(p1<p2)
+            {
+                p1++;
+            }
+            else
+            {
+                p2--;
+            }
+        }
+        for(int num:pair)
+        {
+            System.err.println(num);
+        }
+
+    }
+    public static int min(int a,int b)
+    {
+        if(a>b)
+        {
+            return b;
+        }
+        else
+        {
+            return a;
+        }
 
     }
 }
